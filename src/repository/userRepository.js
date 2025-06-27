c691351d5460c7f56296a0abbef68f8f41d3814b
Himanshu Paliwal <hpaliwal364@gmail.com>
2025-06-27 16:01:09 +0530

    added repository layer

---
+++ src/repository/userRepository.js
@@ -0,0 +1,19 @@
+import User from "../schema/User";
+
+export const getUserByEmail = async (email) => {
+  try {
+    const user = await User.findOne({ email });
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const findAllUsers = async () => {
+  try {
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    console.log(error);
+  }
+};
